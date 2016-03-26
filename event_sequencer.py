7b0428bfa90bdffefe2e062aa8f687683451b1ec
Dani <daniel.cano@doga.es>
2016-03-26 15:42:50 +0100

    cambios en maquina de estados

--- event_sequencer.py
+++ event_sequencer.py
@@ -1,23 +1,51 @@
 #!usr/bin/env/ python
 import RPi.GPIO as GPIO
-import time, sched
+import time
 
 
 def enable():
 	GPIO.output(23, GPIO.HIGH)
 	print "enable output", time.time()
 def disable():
-	GPIO.output(23, GPIO.DOWN)
+	GPIO.output(23, GPIO.LOW)
 	print "disable output", time.time()
+
+def next_state:
+	i = i+1
+	if i>len(list(state_list)):
+		return break
+	else:
+		state = state_list[i][0]
+	        timeout = state_list[i][1]
 
 #Configure
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(24, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)
+GPIO.setup(23, GPIO.OUT)
+GPIO.setup(24, GPIO.OUT)
+GPIO.setup(25, GPIO.OUT)
+GPIO.output(23, GPIO.LOW)
+GPIO.output(24, GPIO.LOW)
+GPIO.output(25, GPIO.LOW)
 
-#Event Queue
-s = sched.scheduler(time.time, time.sleep)
 
-s.enter(5, 1, enable, ())
-s.enter(2, 1, disable, ()
+#State machine
+state_list = [(0,3), (1,5), (2,8)]
 
-print "start time"
+state = 0
+timeout = 0
+i=0
+
+start_time = time.time()
+print "start time: ", start_time
+
+while True:
+	time.sleep(1)
+	if (state ==0):
+		next_state()
+	if ((state == 1) & (time.time()-start_time>timeout)):
+		enable()
+		start_time = time.time()
+	if ((state == 2) & (time.time()-start_time>timeout)):
+		disable()
+		start_time = time.time()
+
